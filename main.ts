f51483b169e21eb1e4720f33f20718ac54a5041d
junhakjang <100299253+junhakjang@users.noreply.github.com>
2022-04-19 10:49:59 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index <= 10; index++) {
+        sum = index + sum
+    }
+    basic.showNumber(sum)
+})
+let sum = 0
+let index = 0
+sum = 0
+basic.forever(function () {
+	
+})
